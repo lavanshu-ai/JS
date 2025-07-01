e10959b2ae8e6053d99a97ad768a6da1c1df7f46
lavanshu-ai <underratediitian@gmail.com>
2025-07-01 16:24:02 +0000

    save me

---
+++ 01_bas/datatype.js
@@ -0,0 +1,24 @@
+let name="lv"  //string
+let phone_no = 94066
+let newname="ba"
+//console.log(typeof newname)
+const id=Symbol("lv")
+const bigINt=32470923472343278402982983744n
+
+console.log(typeof bigINt)
+const heros=["captain america","ironman","01"];
+console.log(typeof heros)
+let myobject={
+    name1:"gabru"
+}
+console.log(typeof myobject)
+const the_fun=function(){
+    console.log("new world")
+}
+
+console.log(typeof the_fun)
+
+
+
+
+
