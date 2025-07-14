ad5c82bab8c7cfcc8e988e7878852b3b0fc9f6ba
lavanshu-ai <underratediitian@gmail.com>
2025-07-14 18:40:19 +0530

    functioning

--- functions/01_bas.js
+++ functions/01_bas.js
@@ -1,4 +1,14 @@
 function  addnum(num1,num2){
-    console.log(num1+num2)
+    return (num1+num2)
 }
-addnum(23,33)
+let result = addnum(23,33)
+console.log("Result :",result )
+
+function UserLoggedinMessage(username){
+    if(!username){
+        return ('Please enter a username')
+    }
+    return `${username} are logged in`
+}
+//console.log(UserLoggedinMessage());
+console.log(UserLoggedinMessage("you"));
