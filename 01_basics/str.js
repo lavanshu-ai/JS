9e794d7a5a841a9e7cd752b53cdd110134fd2c28
lavanshu-ai <underratediitian@gmail.com>
2025-08-01 21:44:36 +0530

    stree

---
+++ 01_basics/str.js
@@ -0,0 +1,11 @@
+let str='lavanshu patidar'
+const surname=str.slice(9,16)
+console.log(surname)
+const name=str.substring(0,8)
+const trial=str.slice(-7)
+const trial=str.slice(-7,-1)
+//slice and substring are used to extract parts of a string
+//slice can take negative indices, substring cannot
+console.log(trial)
+console.log(name)
+
