0339e6d973ccb58f897e1e4382b46c0e48fd15d5
lavanshu-ai <underratediitian@gmail.com>
2025-08-01 21:45:16 +0530

    stillstree

--- 01_basics/str.js
+++ 01_basics/str.js
@@ -3,7 +3,7 @@ const surname=str.slice(9,16)
 console.log(surname)
 const name=str.substring(0,8)
 const trial=str.slice(-7)
-const trial=str.slice(-7,-1)
+const trial1=str.slice(-7,-1)
 //slice and substring are used to extract parts of a string
 //slice can take negative indices, substring cannot
 console.log(trial)
