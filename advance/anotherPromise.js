dfd710cc1785cbf59b3a524d42470d9f5f9dc876
lavanshu-ai <underratediitian@gmail.com>
2025-08-02 15:43:11 +0530

    changes

--- advance/anotherPromise.js
+++ advance/anotherPromise.js
@@ -1,9 +1,10 @@
 function getInfo(){
-    console.log('Fetching info...');
+    console.log('Fetching info...');//1
     return new Promise((resolve) => {
-    console.log('inside promise ...'); 
+    console.log('inside promise ...'); //2
+    // Simulating an asynchronous operation
         const fs = require('fs');
-        
+
         fs.readFile('file_system/info.txt', 'utf-8', (err, data) => {
             console.log('file read');
                 resolve(data);
@@ -16,3 +17,4 @@ function getInfo(){
 let a=getInfo()
 console.log(a);
 a.then(printdata)
+console.log('# check mark');
